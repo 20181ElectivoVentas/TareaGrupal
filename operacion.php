b63dc49edf314978d3c82adcebd9293404ffcd1e
USP <USP@C202-07>
2018-04-27 14:44:19 -0500

    probando

---
+++ operacion.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <form action="suma.php" method="post">
+            <input type="text" name="num1">
+            <p>+</p>
+            <input type="text" name="num2">
+            <input type="submit" value="SUMAR">
+        </form>
+    </body>
+</html>
