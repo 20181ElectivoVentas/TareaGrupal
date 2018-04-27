e7a8393ed2f77ebf215ccfbdc5cfad6177d701ff
USP <USP@C202-07>
2018-04-27 14:56:09 -0500

    probando

--- operacion.php
+++ operacion.php
@@ -1,8 +1,38 @@
 <html>
     <body>
+        
+        <h2>SUMA:</h2>
         <form action="suma.php" method="post">
             <input type="text" name="num1">
             <p>+</p>
+            <input type="text" name="num2">
+            <input type="submit" value="SUMAR">
+        </form>
+        <br>
+        <br>
+         <h2>RESTA:</h2>
+        <form action="resta.php" method="post">
+            <input type="text" name="num1">
+            <p>+</p>
+            <input type="text" name="num2">
+            <input type="submit" value="SUMAR">
+        </form>
+        <br>
+        <br>       
+        <h2>MULTIPLICACIÓN:</h2>
+         <form action="multiplicacion.php" method="post">
+            <input type="text" name="num1">
+            <p>*</p>
+            <input type="text" name="num2">
+            <input type="submit" value="SUMAR">
+        </form>
+        <br>
+        <br>
+                   
+        <h2>DIVISIÓN</h2>
+         <form action="division.php" method="post">
+            <input type="text" name="num1">
+            <p>*</p>
             <input type="text" name="num2">
             <input type="submit" value="SUMAR">
         </form>
